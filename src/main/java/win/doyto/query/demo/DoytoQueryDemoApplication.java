02b74a4bdd55a04dcc13c2afe78d81eeb048d9ad
Forb Yuan <f0rb@163.com>
2021-06-21 20:13:32 +0800

    #card-63512045 Init project

---
+++ src/main/java/win/doyto/query/demo/DoytoQueryDemoApplication.java
@@ -0,0 +1,13 @@
+package win.doyto.query.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoytoQueryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoytoQueryDemoApplication.class, args);
+	}
+
+}
